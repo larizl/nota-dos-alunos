09e701298f03780c73f0ef89e51ff31351095e4f
larizl <161778428+larizl@users.noreply.github.com>
2024-05-24 21:24:04 -0400

    Add files via upload
    negocio das notas dos alunos javascript

---
+++ javascript/recursos/js/app.js
@@ -0,0 +1,17 @@
+console.log(cabecalho)
+
+Function pegarDadosDoAluno(){
+  const nome =document.getElementById("nome")
+  const not1 document.getElementById("nota1")
+  const not2 document.getElementById("nota2")
+  const not3 document.getElementById("nota3")
+  const not4 document.getElementById("nota4")
+  
+console.log(nome.value)
+
+return nome.volue
+}
+
+let pegarDados = pegarDadosDoAluno()
+
+console.log("Nome",pegarDado")
